8584836b5b14bc3c583f1d8494fb054c4a9cda64
Aravind Madana Gopal <amad295@aucklanduni.ac.nz>
2021-09-27 22:42:14 +1300

    Added TSLA,VOO,NIO stock data

--- yahooFinance.py
+++ yahooFinance.py
@@ -17,10 +17,43 @@ def aapl_stock():
     print(values)
 
 
+def tsla_stock():
+    tsla = yf.Ticker("TSLA")
+    tslaInfo = tsla.info
+    keys = ['symbol', 'recommendationKey', 'targetLowPrice', 'targetMedianPrice', 'currentPrice', 'targetMeanPrice']
+    values = list(map(tslaInfo.get, keys))
+    print(values)
+
+
+def voo_stock():
+    voo = yf.Ticker("VOO")
+    vooInfo = voo.info
+    keys = ['symbol', 'recommendationKey', 'targetLowPrice', 'targetMedianPrice', 'currentPrice', 'targetMeanPrice']
+    values = list(map(vooInfo.get, keys))
+    print(values)
+
+
+def nio_stock():
+    nio = yf.Ticker("NIO")
+    nioInfo = nio.info
+    keys = ['symbol', 'recommendationKey', 'targetLowPrice', 'targetMedianPrice', 'currentPrice', 'targetMeanPrice']
+    values = list(map(nioInfo.get, keys))
+    print(values)
+
+
 if __name__ == '__main__':
     print("---- Microsoft Stock Data ----")
     msft_stock()
     print("\n")
     print("---- Apple Stock Data ----")
     aapl_stock()
+    print("\n")
+    print("---- Tesla Stock Data ----")
+    tsla_stock()
+    print("\n")
+    print("---- VOO Stock Data ----")
+    voo_stock()
+    print("\n")
+    print("---- NIO Stock Data ----")
+    nio_stock()
     print("\n")
