536378a4d9d0a757faa4df929251f9e2ca3e3536
Aravind Madana Gopal <amad295@aucklanduni.ac.nz>
2021-09-27 22:27:45 +1300

    Added AAPL stock data

--- yahooFinance.py
+++ yahooFinance.py
@@ -4,10 +4,23 @@ import yfinance as yf
 def msft_stock():
     msft = yf.Ticker("MSFT")
     msftInfo = msft.info
-    keys = ['recommendationKey', 'targetLowPrice', 'targetMedianPrice', 'currentPrice', 'targetMeanPrice']
+    keys = ['symbol', 'recommendationKey', 'targetLowPrice', 'targetMedianPrice', 'currentPrice', 'targetMeanPrice']
     values = list(map(msftInfo.get, keys))
     print(values)
 
 
+def aapl_stock():
+    aapl = yf.Ticker("AAPL")
+    aaplInfo = aapl.info
+    keys = ['symbol', 'recommendationKey', 'targetLowPrice', 'targetMedianPrice', 'currentPrice', 'targetMeanPrice']
+    values = list(map(aaplInfo.get, keys))
+    print(values)
+
+
 if __name__ == '__main__':
+    print("---- Microsoft Stock Data ----")
     msft_stock()
+    print("\n")
+    print("---- Apple Stock Data ----")
+    aapl_stock()
+    print("\n")
