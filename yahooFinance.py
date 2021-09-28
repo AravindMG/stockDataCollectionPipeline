7571b3a81449ad50ef64bfe6f1a76bd4bdb2770c
Aravind Madana Gopal <amad295@aucklanduni.ac.nz>
2021-09-28 22:42:16 +1300

    unable to create dataframe yet

--- yahooFinance.py
+++ yahooFinance.py
@@ -1,59 +1,66 @@
 import yfinance as yf
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StructType, StringType, DoubleType
 
+appName = "Stock dataframe"
+master = "local"
+
+# Create Spark session
+spark = SparkSession.builder \
+    .appName(appName) \
+    .master(master) \
+    .getOrCreate()
 
 def msft_stock():
     msft = yf.Ticker("MSFT")
     msftInfo = msft.info
     keys = ['symbol', 'recommendationKey', 'targetLowPrice', 'targetMedianPrice', 'currentPrice', 'targetMeanPrice']
-    values = list(map(msftInfo.get, keys))
-    print(values)
+    msft_li = list(map(msftInfo.get, keys))
+    return msft_li
 
 
 def aapl_stock():
     aapl = yf.Ticker("AAPL")
     aaplInfo = aapl.info
     keys = ['symbol', 'recommendationKey', 'targetLowPrice', 'targetMedianPrice', 'currentPrice', 'targetMeanPrice']
-    values = list(map(aaplInfo.get, keys))
-    print(values)
+    aapl_li = list(map(aaplInfo.get, keys))
+    return aapl_li
 
 
 def tsla_stock():
     tsla = yf.Ticker("TSLA")
     tslaInfo = tsla.info
     keys = ['symbol', 'recommendationKey', 'targetLowPrice', 'targetMedianPrice', 'currentPrice', 'targetMeanPrice']
-    values = list(map(tslaInfo.get, keys))
-    print(values)
+    tsla_li = list(map(tslaInfo.get, keys))
+    return tsla_li
 
 
-def voo_stock():
-    voo = yf.Ticker("VOO")
-    vooInfo = voo.info
-    keys = ['symbol', 'recommendationKey', 'targetLowPrice', 'targetMedianPrice', 'currentPrice', 'targetMeanPrice']
-    values = list(map(vooInfo.get, keys))
-    print(values)
+# def voo_stock():
+#     voo = yf.Ticker("VOO")
+#     vooInfo = voo.info
+#     keys = ['symbol', 'recommendationKey', 'targetLowPrice', 'targetMedianPrice', 'currentPrice', 'targetMeanPrice']
+#     values = list(map(vooInfo.get, keys))
+#     print(values)
 
 
 def nio_stock():
     nio = yf.Ticker("NIO")
     nioInfo = nio.info
     keys = ['symbol', 'recommendationKey', 'targetLowPrice', 'targetMedianPrice', 'currentPrice', 'targetMeanPrice']
-    values = list(map(nioInfo.get, keys))
-    print(values)
+    nio_li = list(map(nioInfo.get, keys))
+    return nio_li
+
+
+def create_df():
+    msft_data = msft_stock()
+    # tsla_data = tsla_stock()
+    # aapl_data = aapl_stock()
+    # nio_data = nio_stock()
+    columns = ['symbol', 'recommendationKey', 'targetLowPrice', 'targetMedianPrice', 'currentPrice', 'targetMeanPrice']
+    stock_dataframe = spark.createDataFrame([(msft_data,)], columns)
+    stock_dataframe.show()
 
 
 if __name__ == '__main__':
-    print("---- Microsoft Stock Data ----")
-    msft_stock()
-    print("\n")
-    print("---- Apple Stock Data ----")
-    aapl_stock()
-    print("\n")
-    print("---- Tesla Stock Data ----")
-    tsla_stock()
-    print("\n")
-    print("---- VOO Stock Data ----")
-    voo_stock()
-    print("\n")
-    print("---- NIO Stock Data ----")
-    nio_stock()
-    print("\n")
+    create_df()
